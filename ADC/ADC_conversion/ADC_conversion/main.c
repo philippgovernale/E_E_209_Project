dae467816833b1c6c6454351c5fe79d50cb9e91c
Philipp Governale <philipp.governale1@gmail.com>
2019-09-05 23:04:19 +1200

    Fix incorrect variable name bug & return value

--- ADC/ADC_conversion/ADC_conversion/main.c
+++ ADC/ADC_conversion/ADC_conversion/main.c
@@ -38,12 +38,12 @@ uint16_t ADC_convert_channel_to_millivolts(uint8_t channel)
 	uint16_t adc_value = (ADCL << 0) | (ADCH << 8);
 	
 	/* Do we need to do this for every read value? Can't we just do that with our selected ones?*/
-	uint32_t millivolts = (uint32_t)adc_count * (uint32_t)5000 / 1024;
+	uint32_t millivolts = (uint32_t)adc_value * (uint32_t)5000 / 1024;
 	
 	return (uint16_t)millivolts
 }
 
-uint8_t get_peak_voltage()
+uint16_t get_peak_voltage()
 {
 	//uint16_t samples[WAVE_SAMPLES];
 	uint16_t sample;
@@ -53,12 +53,14 @@ uint8_t get_peak_voltage()
 	uint8_t i;
 	for(i=0; i < WAVE_SAMPLES; i++){
 		sample = ADC_convert_channel_to_millivolts(VOLTAGE_CHANNEL)
+		//get maximum amplitude (may be negative part of wave)
 		sample_amplitude = abs(VOLTAGE_OFFSET - sample);
 		
 		if (sample_amplitude > max_sample_amplitude){
 			max_sample_amplitude = sample_amplitude;
 		}
-	} 
+	}
+	return max_sample_amplitude;
 }
 
 int main(void)
