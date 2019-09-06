63815770e3d2438152150bc2891e534f02e96f2d
Philipp Governale <philipp.governale1@gmail.com>
2019-09-06 14:50:51 +1200

    Altered initialisation register values

--- ADC/ADC_conversion/ADC_conversion/main.c
+++ ADC/ADC_conversion/ADC_conversion/main.c
@@ -8,15 +8,21 @@
 #include <avr/io.h>
 #include <stdlib.h>
 
-#define WAVE_SAMPLES 19
+#define WAVE_SAMPLES 19 //for 52 us sampling rate
 #define VOLTAGE_OFFSET 1760
-#define VOLTAGE_CHANNEL X // needs to be defined!
+#define VOLTAGE_CHANNEL 0x40 // Channel 0 Port C0
+#define CURRENT_CHANNEL 0x45 // Channel 5 Port C5
 
 void ADC_initialise(void)
 {
-	ADMUX = 0x40;
-	ADCSRA = 0x87;
+	ADMUX = 0x40; //set to voltage channel by default
+	ADCSRA = 0x86; //prescaler to 64
 	ADCSRB = 0x00;
+}
+
+void ADC_set_channel(uint8_t channel)
+{
+	ADMUX = channel;
 }
 
 //converts ADC and returns result in millivolts
@@ -40,7 +46,7 @@ uint16_t ADC_convert_channel_to_millivolts(uint8_t channel)
 	/* Do we need to do this for every read value? Can't we just do that with our selected ones?*/
 	uint32_t millivolts = (uint32_t)adc_value * (uint32_t)5000 / 1024;
 	
-	return (uint16_t)millivolts
+	return (uint16_t)millivolts;
 }
 
 uint16_t get_peak_voltage()
@@ -51,12 +57,12 @@ uint16_t get_peak_voltage()
 	uint16_t max_sample_amplitude = 0;
 	
 	uint8_t i;
-	for(i=0; i < WAVE_SAMPLES; i++){
-		sample = ADC_convert_channel_to_millivolts(VOLTAGE_CHANNEL)
+	for (i=0; i < WAVE_SAMPLES; i++) {
+		sample = ADC_convert_channel_to_millivolts(VOLTAGE_CHANNEL);
 		//get maximum amplitude (may be negative part of wave)
 		sample_amplitude = abs(VOLTAGE_OFFSET - sample);
 		
-		if (sample_amplitude > max_sample_amplitude){
+		if (sample_amplitude > max_sample_amplitude) {
 			max_sample_amplitude = sample_amplitude;
 		}
 	}
